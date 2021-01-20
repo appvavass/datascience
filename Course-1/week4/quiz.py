a17fd82654bb02e42a7f3ecbd0bb8b746a665972
Vavassori Giacomo <giacomo.vavassori@scania.com>
2021-01-20 16:27:56 +0100

    plot created

--- Course-1/week4/quiz.py
+++ Course-1/week4/quiz.py
@@ -1,15 +1,11 @@
 import re
+s = 'ACAABAACAAABACDBADDDFSDDDFFSSSASDAFAAACBAAAFASD'
 
-def result():
-    s = 'ACAABAACAAABACDBADDDFSDDDFFSSSASDAFAAACBAAAFASD'
+result = []
+# compete the pattern below
+pattern = '([B-Z])AAA'
+print(re.finditer(pattern,s))
+for item in re.finditer(pattern, s):
+# identify the group number below.
+  result.append(item.group())
 
-    result = []
-    # compete the pattern below
-    pattern = '([A-Z]*?)AAA'
-    for item in re.finditer(pattern, s):
-      # identify the group number below.
-      result.append(item.group())
-      
-    return result
-
-print(result())
